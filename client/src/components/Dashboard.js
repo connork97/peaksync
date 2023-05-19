ec86ba3462621e8c51ada48c3c5beb2afaf8763c
Connor Kormos <connorkormos97@gmail.com>
2023-05-19 13:42:47 -0700

    filtering functionality for user database complete

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -7,13 +7,14 @@ import { render } from "react-dom"
 // import ListGroup from "react-bootstrap/ListGroup"
 import ListGroup from 'react-bootstrap/ListGroup';
 import Table from 'react-bootstrap/Table'
+import Dropdown from "react-bootstrap/Dropdown"
 
 const Dashboard = () => {
 
     const [currentUser, setCurrentUser] = useRecoilState(userState)
     const [allUsers, setAllUsers] = useState([])
     const [searchParams, setSearchParams] = useState("")
-    const [searchCategeory, setSearchCategory] = useState("")
+    const [searchCategory, setSearchCategory] = useState("Filter By")
 
     const [activeLi, setActiveLi] = useState("")
 
@@ -27,65 +28,110 @@ const Dashboard = () => {
     const columnLabels = ["ID", "Last Name", "First Name", "Email", "Phone Number", "Waiver", "Address", "City", "State", "Zipcode", "Date of Birth", "Created At"]
 
     const renderColumnLabels = columnLabels.map((label) => {
-        return <td>{label} --- </td>
+        return <th>{label}</th>
     })
 
-    const handleClickedLi = (id) => {
-        setActiveLi(id)
-        console.log(id)
-    }
+    const renderDropdownItems = columnLabels.map((label) => {
+        return (
+            <Dropdown.Item name={label} onClick={(event) => setSearchCategory(event.target.name)}>{label}</Dropdown.Item>
+        )
+    })
 
     const handleDoubleClick = (itemId) => {
         console.log(`Item ${itemId} was double-clicked.`);
       };
 
+    const convertSearchCategory = () => {
+        let splitSearchCategory = searchCategory.split(" ")
+        let joinedSearchCategory = splitSearchCategory.join("_")
+        let convertedSearchCategory = joinedSearchCategory.toLowerCase()
+        console.log(convertedSearchCategory)
+        return convertedSearchCategory
+    }
+
     let i = 0
     const renderAllUsers = allUsers.map((user) => {
         i++
-        return (
-            <>
-                {/* <ListGroup.Item key={user.id} onClick={() => handleClickedLi(user.id)} onDoubleClick={() => handleDoubleClick(user.id)} active={activeLi === user.id}> */}
-                <tr key={user.id} onClick={() =>handleClickedLi(user.id)} style={{background: activeLi === user.id ? "lightblue" : null}}>
-                    <td>{user.id}</td>
-                    <td>{user.last_name}</td>
-                    <td>{user.first_name}</td>
-                    <td>{user.email}</td>
-                    <td>{user.phone_number}</td>
-                    <td>{user.waiver ? "True" : "False"}</td>
-                    <td>{user.address}</td>
-                    <td>{user.city}</td>
-                    <td>{user.state}</td>
-                    <td>{user.zipcode}</td>
-                    <td>{user.date_of_birth}</td>
-                    <td>{user.created_at}</td>
-                </tr>
-                {/* </ListGroup.Item> */}
-            </>
+        // convertSearchCategory()
+        // if (searchCategory !== "Filter By" && user.convertedSearchCategory.includes(searchParams.toLowerCase())) {
+            return (
+                <>
+            {/* <ListGroup.Item key={user.id} onClick={() => handleClickedLi(user.id)} onDoubleClick={() => handleDoubleClick(user.id)} active={activeLi === user.id}> */}
+            <tr key={user.id} onClick={() => setActiveLi(user.id)} style={{background: activeLi === user.id ? "lightblue" : null}}>
+                <td>{user.id}</td>
+                <td>{user.last_name}</td>
+                <td>{user.first_name}</td>
+                <td>{user.email}</td>
+                <td>{user.phone_number}</td>
+                <td>{user.waiver ? "True" : "False"}</td>
+                <td>{user.address}</td>
+                <td>{user.city}</td>
+                <td>{user.state}</td>
+                <td>{user.zipcode}</td>
+                <td>{user.date_of_birth}</td>
+                <td>{user.created_at}</td>
+            </tr>
+        </>
         )
+    // }
     })
 
+    let convertedSearchCategory = searchCategory.split(" ").join("_").toLowerCase()
+    console.log(convertedSearchCategory)
+
+    // const filterUsers = (category) => allUsers.filter((user) => user[category].toLowerCase().includes(searchParams.toLowerCase()))
+    // filterUsers(searchCategory)
+    const filterUsers = (category) => allUsers.map((user) => {
+        i++
+        console.log(category)
+        console.log(searchParams)
+        // convertSearchCategory()
+        // if (searchCategory !== "Filter By" && user.convertedSearchCategory.includes(searchParams.toLowerCase())) {
+        if (String(user[category]).toLowerCase().includes(String(searchParams).toLowerCase())) {
+            return (
+                <>
+            {/* <ListGroup.Item key={user.id} onClick={() => handleClickedLi(user.id)} onDoubleClick={() => handleDoubleClick(user.id)} active={activeLi === user.id}> */}
+            <tr key={user.id} onClick={() => setActiveLi(user.id)} style={{background: activeLi === user.id ? "lightblue" : null}}>
+                <td>{user.id}</td>
+                <td>{user.last_name}</td>
+                <td>{user.first_name}</td>
+                <td>{user.email}</td>
+                <td>{user.phone_number}</td>
+                <td>{user.waiver ? "True" : "False"}</td>
+                <td>{user.address}</td>
+                <td>{user.city}</td>
+                <td>{user.state}</td>
+                <td>{user.zipcode}</td>
+                <td>{user.date_of_birth}</td>
+                <td>{user.created_at}</td>
+            </tr>
+        </>
+        )
+    }
+    // }
+    })
     return (
         <>
             <h1>Dashboard Page</h1>
+            <div id="adminSearchDiv" style={{display: "flex"}}>
+            <input type="text" value={searchParams} onChange={(event) => setSearchParams(event.target.value)}></input>
+            <Dropdown>
+                <Dropdown.Toggle>
+                    {searchCategory}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    {renderDropdownItems}
+                </Dropdown.Menu>
+            </Dropdown>
+            </div>
             <Table id="adminDashboardTable">
                 <thead>
                     <tr>
-                        <th>ID</th>
-                        <th>Last Name</th>
-                        <th>First Name</th>
-                        <th>Email</th>
-                        <th>Phone Number</th>
-                        <th>Waiver</th>
-                        <th>Address</th>
-                        <th>City</th>
-                        <th>State</th>
-                        <th>Zipcode</th>
-                        <th>Date Of Birth</th>
-                        <th>Created At</th>
+                        {renderColumnLabels}
                     </tr>
                 </thead>
                 <tbody>
-                    {renderAllUsers}
+                    {searchCategory === "Filter By" ? renderAllUsers : filterUsers(convertedSearchCategory)}
                 </tbody>
             </Table>
         </>
