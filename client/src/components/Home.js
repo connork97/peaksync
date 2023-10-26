9a1ed1bf260619eb248510689864093a67920628
Connor Kormos <connorkormos97@gmail.com>
2023-10-26 14:24:03 -0700

    fixed but for loading indicator when no user is logged in

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -13,11 +13,11 @@ const Home = () => {
     const [loadingText, setLoadingText] = useState('');
 
     useEffect(() => {
-        setLoadingText('Checking user privileges...')
+        if (localStorage.getItem("user_id")) setLoadingText('Checking user privileges...')
     }, [])
 
     useEffect(() => {
-        if (Object.keys(currentUser).length > 0) {
+        if (Object.keys(currentUser).length > 0 || !localStorage.getItem("user_id")) {
             if (allEvents.length <= 0) setLoadingText('Retrieving calendar items...');
             else if (allMemberships.length <= 0) setLoadingText('Retrieving membership offerings...');
             else if (allSessions.length <= 0) setLoadingText('Retrieving event signups...');
@@ -29,6 +29,8 @@ const Home = () => {
         <div className="mainDiv">
             <br></br><br></br><br></br><br></br><br></br>
             <h1 className={styles.welcomeH1}>Welcome to PeakSync {currentUser.first_name}!</h1>
+            {loadingText !== "You're ready to go!" && <h2 className={styles.loadedH2}>Please allow the web service to spin up.</h2>}   
+            {loadingText !== "You're ready to go!" && <h2 className={styles.loadedH2}>This may take up to one minute.</h2>}
             <h2 className={loadingText === "You're ready to go!" ? styles.loadedH2 : styles.loadingH2}>{loadingText}</h2>
         </div>
     )
